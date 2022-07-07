7589e9eb48250b6a48b4898f45e9cb165fce8f12
Marcel Planas <marcel@iestrategic.com>
2022-07-07 12:46:41 +0200

    Converted index and setupTests to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
-const container = document.getElementById('root');
+const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
